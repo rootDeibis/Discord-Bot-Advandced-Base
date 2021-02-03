db4a2220bc4a78a13be0bb90f7c93d6bdcde4b21
Deibis Alejandro <35423510+rootDeibis@users.noreply.github.com>
2021-02-03 09:48:18 -1200

    Update ready.event.js

--- src/bot/events/ready.event.js
+++ src/bot/events/ready.event.js
@@ -3,12 +3,38 @@ const Colors = require('colors'); //# https://www.npmjs.com/package/colors
 module.exports = {
 	name: "ready",
 	call: (Event, Import) => {
+		var ClientUser = Import.Client.user;
 		
-		console.clear();
+		console.clear(); //# Clear Console
+
 
 		var Prefix = "[Discord-Bot] ".brightCyan;
-
 		console.log(Prefix + "Bot is ready!");
 
+
+		//# Change Bot Status
+
+		//# List: online, idle, invisible, dnd 
+
+		ClientUser.setStatus('idle') 
+		.catch(console.log);
+
+
+
+		//# Change Bot Activity
+
+		ClientUser.setActivity('Example Discord Bot Advanced Base',{
+				/*
+					If type is 'STREAMING':
+						url: 'STREAM URL YOUTUBE/TWICTH',
+						type: 'STREAMING'
+				*/
+				type: "WATCHING" //# List: PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
+
+		}) 
+		.catch(console.log);
+
+		
+		
 	}
 }
