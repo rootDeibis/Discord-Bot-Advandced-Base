2400de1686cd70b631535a8bb553f0595cea34bc
Deibis Alejandro <35423510+rootDeibis@users.noreply.github.com>
2021-02-03 09:19:48 -1200

    Create ready.event.js

---
+++ src/bot/events/ready.event.js
@@ -0,0 +1,15 @@
+const Colors = require('colors'); //# https://www.npmjs.com/package/colors
+
+module.exports = {
+	name: "ready",
+	call: (Event, Import) => {
+		
+		console.clear();
+
+		var Prefix = "[Discord-Bot] ".brightCyan;
+
+		console.log(Prefix + "Bot is ready!");
+;
+
+	}
+}
