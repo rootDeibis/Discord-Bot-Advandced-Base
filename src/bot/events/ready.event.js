82c43e21276c07606e396277e777917664aa5898
Deibis Alejandro <35423510+rootDeibis@users.noreply.github.com>
2021-02-03 09:51:33 -1200

    Update ready.event.js

--- src/bot/events/ready.event.js
+++ src/bot/events/ready.event.js
@@ -3,6 +3,7 @@ const Colors = require('colors'); //# https://www.npmjs.com/package/colors
 module.exports = {
 	name: "ready",
 	call: (Event, Import) => {
+		var ClientSettings = Import.Config;
 		var ClientUser = Import.Client.user;
 		
 		console.clear(); //# Clear Console
@@ -16,20 +17,20 @@ module.exports = {
 
 		//# List: online, idle, invisible, dnd 
 
-		ClientUser.setStatus('idle') 
+		ClientUser.setStatus(ClientSettings.bot.status) 
 		.catch(console.log);
 
 
 
 		//# Change Bot Activity
 
-		ClientUser.setActivity('Example Discord Bot Advanced Base',{
+		ClientUser.setActivity(ClientSettings.bot.activity.content,{
 				/*
 					If type is 'STREAMING':
 						url: 'STREAM URL YOUTUBE/TWICTH',
 						type: 'STREAMING'
 				*/
-				type: "WATCHING" //# List: PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
+				type: ClientSettings.bot.activity.type //# List: PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
 
 		}) 
 		.catch(console.log);
