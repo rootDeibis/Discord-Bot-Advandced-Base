8d9fa7f669fbaa2e93e079bcca81e7c8a00d79ff
Deibis Alejandro <35423510+rootDeibis@users.noreply.github.com>
2021-02-03 11:07:15 -1200

    Update Strings.js

--- src/extends/Strings.js
+++ src/extends/Strings.js
@@ -7,5 +7,20 @@ module.exports = {
 			  return false;
 			}
 		};
+
+		String.prototype.utilSearch = function(search) {
+			var Regex = new RegExp(`${search}="(.*?)"`,'g');
+			var array = new String(this).match(Regex);
+			var Result = null;
+
+			if (array == undefined) return Result;
+
+			for(i = 0; i < array.length; i++) {
+				var RegexR =  RegExp(/(["'])(?:(?=(\\?))\2.)*?\1/g,'g');
+ 				Result = array[i].match(RegexR, '')[0].replace(/"/g, '');
+			}
+
+			return Result;
+		};
 	}
 }
